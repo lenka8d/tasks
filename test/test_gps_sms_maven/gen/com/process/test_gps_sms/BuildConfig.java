052f86ce3f407544fc18b2185b22ba6d35c03947
lenka8d <lenka8d@gmail.com>
2013-06-05 11:09:31 +0200

    add test

---
+++ test/test_gps_sms_maven/gen/com/process/test_gps_sms/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.process.test_gps_sms;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ test/test_gps_sms_maven/target/generated-sources/r/com/process/test_gps_sms/BuildConfig.java
@@ -0,0 +1,5 @@
+package com.process.test_gps_sms;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = true;
+}
